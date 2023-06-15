b936c8c8f7835b83349e206f596bf8d691576422
linh.truongthe <linh.truongthe@lazada.com>
2023-06-15 22:47:00 +0700

    Working on Game Teris.

--- src/main/resources/static/js/teris-game.js
+++ src/main/resources/static/js/teris-game.js
@@ -1,4 +1,29 @@
 var gameBoard = document.getElementById("game-board");
+var resultMessage = document.getElementById("result-message");
+
+// Mảng chứa các loại khối
+var blockTypes = ["red", "blue"];
+
+// Khối hiện tại đang xuất hiện trong game board
+var currentBlock = null;
+
+// Tọa độ hàng và cột của khối hiện tại
+var currentRow = 0;
+var currentCol = 0;
+
+// Hàm tạo một khối mới và cho nó xuất hiện trong game board
+function createBlock() {
+  // Chọn một loại khối ngẫu nhiên
+  var randomBlockType = blockTypes[Math.floor(Math.random() * blockTypes.length)];
+
+  // Thiết lập khối hiện tại
+  currentBlock = randomBlockType;
+  currentRow = 0;
+  currentCol = Math.floor(Math.random() * 7); // Vị trí ngẫu nhiên trong khoảng từ 0 đến 6
+
+  // Đặt giá trị màu sắc cho ô trong game matrix
+  setBlockColor(currentRow, currentCol, currentBlock);
+}
 
 // Tạo ma trận game với 20 hàng và 10 cột
 var gameMatrix = [];
@@ -25,141 +50,244 @@ function drawGame() {
         block.classList.add("blue");
       }
 
+      // Kiểm tra vị trí của ô vuông trong ma trận game
+      // và thêm lớp "row-divider" và "column-divider" tương ứng
+      if (i !== 19) {
+        block.classList.add("row-divider");
+      }
+      if (j !== 9) {
+        block.classList.add("column-divider");
+      }
+
       gameBoard.appendChild(block);
     }
   }
 }
+
 
 // Đặt giá trị màu sắc cho ô trong ma trận game
 function setBlockColor(row, col, color) {
   gameMatrix[row][col] = color;
 }
 
-// Hàm tạo một khối vuông đỏ 2x2
-function createRedBlock() {
-  setBlockColor(0, 0, "red");
-  setBlockColor(0, 1, "red");
-  setBlockColor(1, 0, "red");
-  setBlockColor(1, 1, "red");
+// Kiểm tra xem một khối có gây va chạm hay không
+function isCollision(row, col, blockType) {
+  // Kiểm tra va chạm với khối đã xếp và viền màn hình
+  if (
+    row < 0 ||
+    row >= 20 ||
+    col < 0 ||
+    col >= 10 ||
+    gameMatrix[row][col] !== null
+  ) {
+    return true;
+  }
+
+  return false;
 }
 
-// Hàm tạo một khối vuông xanh 3x3
-function createBlueBlock() {
-  setBlockColor(0, 0, "blue");
-  setBlockColor(0, 1, "blue");
-  setBlockColor(0, 2, "blue");
-  setBlockColor(1, 0, "blue");
-  setBlockColor(1, 1, "blue");
-  setBlockColor(1, 2, "blue");
-  setBlockColor(2, 0, "blue");
-  setBlockColor(2, 1, "blue");
-  setBlockColor(2, 2, "blue");
-}
+// Di chuyển khối vuông sang trái
+function moveLeft() {
+  var canMove = true;
+  for (var i = 0; i < 20; i++) {
+    for (var j = 0; j < 10; j++) {
+      if (gameMatrix[i][j] === "red") {
+        if (isCollision(i, j - 1, "red")) {
+          canMove = false;
+          break;
+        }
+      } else if (gameMatrix[i][j] === "blue") {
+        if (isCollision(i, j - 1, "blue")) {
+          canMove = false;
+          break;
+        }
+      }
+    }
+    if (!canMove) {
+      break;
+    }
+  }
 
-
-var score = 0; // Biến lưu trữ điểm số của người chơi
-
-// Hàm tính điểm khi xóa hàng
-function calculateScore(rowsCleared) {
-  // Cập nhật điểm số dựa trên số hàng đã xóa
-  score += rowsCleared * 100;
-}
-
-// Hàm tính điểm khi xếp khối
-function calculateScoreForBlock(blockType) {
-  // Cập nhật điểm số dựa trên loại khối đã xếp
-  if (blockType === "red") {
-    score += 50;
-  } else if (blockType === "blue") {
-    score += 75;
+  if (canMove) {
+    for (var i = 0; i < 20; i++) {
+      for (var j = 0; j < 10; j++) {
+        if (gameMatrix[i][j] === "red") {
+          gameMatrix[i][j - 1] = "red";
+          gameMatrix[i][j] = null;
+        } else if (gameMatrix[i][j] === "blue") {
+          gameMatrix[i][j - 1] = "blue";
+          gameMatrix[i][j] = null;
+        }
+      }
+    }
+    drawGame();
   }
 }
 
-// Hàm tính điểm khi xếp khối đặc biệt
-function calculateSpecialScore() {
-  // Cập nhật điểm số cho việc xếp khối đặc biệt
-  score += 200;
-}
 
-// Hàm tính điểm cho thời gian chơi
-function calculateScoreForTime(timePlayed) {
-  // Cập nhật điểm số dựa trên thời gian chơi
-  // Ví dụ: Cứ mỗi 1 phút chơi, người chơi nhận được 10 điểm
-  var minutesPlayed = Math.floor(timePlayed / 60000);
-  var scoreForTime = minutesPlayed * 10;
-  score += scoreForTime;
-}
-
-// Hàm tính điểm
-function calculateScore() {
-  // Code tính điểm của trò chơi Teris
-
-  // In điểm số ra console hoặc hiển thị trên giao diện
-  console.log("Điểm số: " + score);
-
-  submitScore(score);
-
-}
-
-// Hàm gửi kết quả lên server qua Ajax
-function submitScore(score) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", '/voucher/' + voucherId + '/redeem', true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  var csrfToken = $("input[name='_csrf']").val();
-  xhr.setRequestHeader("X-CSRF-TOKEN", csrfToken);
-  xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-          var response = JSON.parse(xhr.responseText);
-          console.log(response);
-
-          if (response.win === true) {
-              resultMessage.textContent = response.winMessage;
-          } else {
-              resultMessage.textContent = response.failedErrorMessage;
-          }
-          resultMessage.style.display = "block";
+// Di chuyển khối vuông sang phải
+function moveRight() {
+  var canMove = true;
+  for (var i = 0; i < 20; i++) {
+    for (var j = 9; j >= 0; j--) {
+      if (gameMatrix[i][j] === "red") {
+        if (isCollision(i, j + 1, "red")) {
+          canMove = false;
+          break;
+        }
+      } else if (gameMatrix[i][j] === "blue") {
+        if (isCollision(i, j + 1, "blue")) {
+          canMove = false;
+          break;
+        }
       }
-  };
+    }
+    if (!canMove) {
+      break;
+    }
+  }
 
-  var gamePlayDTO = {
-                    gameType: gameType,
-                    playedAt: new Date().getTime(),
-                    playData: score
-                       };
-  xhr.send(JSON.stringify(gamePlayDTO));
+  if (canMove) {
+    for (var i = 0; i < 20; i++) {
+      for (var j = 9; j >= 0; j--) {
+        if (gameMatrix[i][j] === "red") {
+          gameMatrix[i][j + 1] = "red";
+          gameMatrix[i][j] = null;
+        } else if (gameMatrix[i][j] === "blue") {
+          gameMatrix[i][j + 1] = "blue";
+          gameMatrix[i][j] = null;
+        }
+      }
+    }
+    drawGame();
+  }
 }
 
+// Di chuyển khối vuông xuống dưới
+function moveDown() {
+  var canMove = true;
+  for (var i = 19; i >= 0; i--) {
+    for (var j = 0; j < 10; j++) {
+      if (gameMatrix[i][j] === "red") {
+        if (isCollision(i + 1, j, "red")) {
+          canMove = false;
+          break;
+        }
+      } else if (gameMatrix[i][j] === "blue") {
+        if (isCollision(i + 1, j, "blue")) {
+          canMove = false;
+          break;
+        }
+      }
+    }
+    if (!canMove) {
+      break;
+    }
+  }
 
-// Hàm kết thúc trò chơi
-function endGame() {
-  // Dừng trò chơi hoặc hiển thị thông báo kết thúc
-
-  // Tính điểm
-  calculateScore();
+  if (canMove) {
+    for (var i = 19; i >= 0; i--) {
+      for (var j = 0; j < 10; j++) {
+        if (gameMatrix[i][j] === "red") {
+          gameMatrix[i + 1][j] = "red";
+          gameMatrix[i][j] = null;
+        } else if (gameMatrix[i][j] === "blue") {
+          gameMatrix[i + 1][j] = "blue";
+          gameMatrix[i][j] = null;
+        }
+      }
+    }
+    drawGame();
+  }
 }
 
-// Hàm khởi động trò chơi
-function startGame() {
-  // Code khởi tạo và bắt đầu trò chơi Teris
-
-  // Đặt thời gian đếm ngược là 5 phút (300,000ms)
-  var duration = 300000;
-
-  // Thiết lập thời gian đếm ngược
-  setTimeout(function() {
-    // Kết thúc trò chơi sau khi hết thời gian
-    endGame();
-  }, duration);
+// Hàm kiểm tra xem khối đã đạt đến đáy chưa
+function isBlockAtBottom() {
+  for (var i = 19; i >= 0; i--) {
+    for (var j = 0; j < 10; j++) {
+      if (
+        gameMatrix[i][j] === "red" ||
+        gameMatrix[i][j] === "blue"
+      ) {
+        if (i === 19 || gameMatrix[i + 1][j] !== null) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
 
-// Khởi tạo game với các khối vuông
-function initializeGame() {
-  createRedBlock();
-  createBlueBlock();
-  drawGame();
-  startGame();
-  calculateScore();
-}
+  // Hàm xử lý khi khối chạm đáy
+  function handleBlockAtBottom() {
+    for (var i = 0; i < 20; i++) {
+      for (var j = 0; j < 10; j++) {
+        if (gameMatrix[i][j] === "red") {
+          gameMatrix[i][j] = null;
+        } else if (gameMatrix[i][j] === "blue") {
+          gameMatrix[i][j] = null;
+        }
+      }
+    }
 
-window.addEventListener("load", initializeGame);
+    // Tính điểm và hiển thị kết quả
+    var score = parseInt(resultMessage.textContent);
+    score += 10;
+    resultMessage.textContent = score;
+
+    // Kiểm tra xem có hàng nào đã đầy không
+    for (var i = 19; i >= 0; i--) {
+      var isFullRow = true;
+      for (var j = 0; j < 10; j++) {
+        if (gameMatrix[i][j] === null) {
+          isFullRow = false;
+          break;
+        }
+      }
+
+      if (isFullRow) {
+        // Xóa hàng đầy
+        for (var k = i; k > 0; k--) {
+          for (var j = 0; j < 10; j++) {
+            gameMatrix[k][j] = gameMatrix[k - 1][j];
+          }
+        }
+        for (var j = 0; j < 10; j++) {
+          gameMatrix[0][j] = null;
+        }
+
+        // Tăng điểm khi xóa hàng
+        score += 100;
+        resultMessage.textContent = score;
+      }
+    }
+
+    drawGame();
+  }
+
+  // Hàm bắt đầu trò chơi
+  function startGame() {
+    createBlock(); // Tạo một khối mới
+    drawGame(); // Vẽ game board
+
+    setInterval(function () {
+      if (!isBlockAtBottom()) {
+        moveDown();
+      } else {
+        handleBlockAtBottom();
+        createBlock(); // Tạo một khối mới khi khối hiện tại đạt đến đáy
+      }
+    }, 1000);
+
+    // Lắng nghe sự kiện nhấn phím để di chuyển khối vuông
+    document.addEventListener("keydown", function (event) {
+      if (event.code === "ArrowLeft") {
+        moveLeft();
+      } else if (event.code === "ArrowRight") {
+        moveRight();
+      }
+    });
+  }
+
+  // Gọi hàm bắt đầu trò chơi khi trang web được tải
+startGame();
