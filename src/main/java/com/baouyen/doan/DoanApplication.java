9e57c06d7acdca544635c0a16347c07dde8c4966
linh.truongthe <linh.truongthe@lazada.com>
2023-06-17 21:18:48 +0700

    Remove un-used code.

--- src/main/java/com/baouyen/doan/DoanApplication.java
+++ src/main/java/com/baouyen/doan/DoanApplication.java
@@ -42,108 +42,11 @@ public class DoanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoanApplication.class, args);
-
 	}
 
 	@PostConstruct
 	public void setupDbWithData() {
-		/*
-		createCampaign();
-		createCampaign();
-		createCampaign();
-		createCampaign();
-		createCampaign();
-		createCampaignNoVoucher();
-		 */
-
 		createGame();
-		/*
-		createStore();
-		createCampaignNoVoucher();
-		 */
-	}
-
-	private void createStore() {
-		Store store = new Store();
-		store.setType(StoreDto.StoreType.OFFLINE);
-		store.setName("store name");
-
-		Partner partner = partnerRepository.findByName("partner1").get(0);
-		store.setPartner(partner);
-		store.setLatitude(1.0);
-		store.setLongitude(2.0);
-		store.setOfflineAddress("offline address");
-		storeRepository.save(store);
-	}
-
-	private void createCampaignNoVoucher() {
-		Campaign result = new Campaign();
-		result.setName("name " + generateRandomString(2));
-
-		int month = new Random().nextInt(4) + 7;
-		LocalDate startDate = LocalDate.of(2023, month, 28);
-		LocalDate endDate = LocalDate.of(2023, month, 30);
-
-		result.setStartDate(startDate);
-		result.setEndDate(endDate);
-
-		Partner createdPartner = createPartner();
-		result.setPartner(createdPartner);
-
-		Game game = gameRepository.findByIdIn(Arrays.asList(2L)).get(0);
-		result.setGames(new HashSet<>(Collections.singletonList(game)));
-		result.setStatus(CampaignStatus.INITIAL);
-
-		Campaign save = campaignRepository.save(result);
-	}
-
-	private void createCampaign() {
-		Campaign result = new Campaign();
-		result.setName("name " + generateRandomString(2));
-
-		int month = new Random().nextInt(4) + 5;
-		LocalDate startDate = LocalDate.of(2023, month, 22);
-		LocalDate endDate = LocalDate.of(2023, month, 30);
-
-		result.setStartDate(startDate);
-		result.setEndDate(endDate);
-
-		Partner createdPartner = createPartner();
-		result.setPartner(createdPartner);
-
-		// init voucher
-		createVoucher(result);
-		createGame(result);
-
-		result.setStatus(CampaignStatus.CREATED_VOUCHERS);
-
-		Campaign save = campaignRepository.save(result);
-	}
-
-	private static void createVoucher(Campaign result) {
-		Voucher voucher = new Voucher();
-		voucher.setDescription("voucher description " + generateRandomString(2));
-		voucher.setCode(UUID.randomUUID().toString());
-		voucher.setType(TEN_PERCENT_DIS_COUNT);
-		voucher.setStatus(VoucherDto.VOUCHER_STATUS.INITIAL);
-
-		voucher.setGameRandomNumber(String.valueOf(RandomUtil.generateRandomNumber(6))
-				.toUpperCase());
-		result.setVouchers(new HashSet<>(Collections.singletonList(voucher)));
-	}
-
-	private static void createGame(Campaign result) {
-		Set<Game> games = new HashSet<>();
-
-		for(int i=0; i<5; i++) {
-			Game game = new Game();
-			int gameTypeInt = new Random().nextInt(2);
-			game.setName("game " + generateRandomString(2));
-			game.setGameType(GameType.values()[gameTypeInt]);
-			games.add(game);
-		}
-
-		result.setGames(games);
 	}
 
 	/**
@@ -157,17 +60,5 @@ public class DoanApplication {
 			gameRepository.save(game);
 		}
 	}
-
-	private Partner createPartner() {
-		Partner partner = new Partner();
-		partner.setName("partner name " + generateRandomString(2));
-		partner.setProvinceAddress("province " + generateRandomString(2));
-		partner.setWardAddress("ward" + generateRandomString(2));
-		partner.setDistrictAddress("district " + generateRandomString(2));
-		partner.setStreetAddress("street " + generateRandomString(2));
-		Partner createdPartner = partnerRepository.save(partner);
-		return createdPartner;
-	}
-
 
 }
